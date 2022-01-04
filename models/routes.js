43ef372831cd43de0a6280d519bda47b954c3f0c
wijoayal <wijoayal@espol.edu.ec>
2022-01-03 19:30:28 -0500

    updated variable

--- models/routes.js
+++ models/routes.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const uri = "mongodb+srv://root:root@cluster0.6b2ol.mongodb.net/router_db?retryWrites=true&w=majority";
 
-mongoose.connect(process.env.uri || 'mongodb://localhost:27017/router_db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/router_db', {
     useNewUrlParser: true,
 },
 (err) => {
